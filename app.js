7d2814698cca4a30ece338af312b3be3489081cf
Sebastian Castañeda <sebastiansnipez@gmail.com>
2023-01-05 16:24:56 -0500

    .env variables changed

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const express = require("express")
-const cors = require("cors")
-const { PORT, FRONT_URL } = require("./config")
+const { PORT } = require("./config")
 const v1 = require("./src/v1/app")
 const app = express()
 
 app.use(express.json())
-
-app.use(cors({
-    origin: FRONT_URL
-}))
 
 const root = "/api"
 
--- config/index.js
+++ config/index.js
@@ -3,7 +3,6 @@ require("dotenv").config()
 module.exports = {
     PORT: process.env.PORT,
     API_KEY: process.env.API_KEY,
-    FRONT_URL: process.env.FRONT_URL,
     MICROSERVICE_A_URL: process.env.MICROSERVICE_A_URL,
     PRODUCTION: process.env.NODE_ENV === "production",
 }
